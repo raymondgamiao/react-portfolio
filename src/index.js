6bee83fb2b60e0037d2bc15d80cc4135556705a8
raymondgamiao <royalrfgum@gmail.com>
2022-08-22 00:17:27 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 import "./index.css";
-import App from "./App";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,23 +2,16 @@ import React, { useCallback } from "react";
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
 
-import "./App.css";
-import particlesOptions from "./particles.json";
+import "./Home.css";
+import particlesOptions from "../particles.json";
 
-function App() {
+const Home = () => {
   const particlesInit = useCallback((main) => {
     loadFull(main);
   }, []);
-
   return (
-    <div className="App">
+    <>
       <Particles options={particlesOptions} init={particlesInit} />
-      <ul>
-        <li>home</li>
-        <li>projects</li>
-        <li>contacts</li>
-        <li> navbar dito</li>
-      </ul>
 
       <h1>Hi! I'm MONDI</h1>
       <h2>Fullstack Web Developer</h2>
@@ -33,8 +26,8 @@ function App() {
         <li>facebook</li>
         <li>social link icons sa gilid</li>
       </ul>
-    </div>
+    </>
   );
-}
+};
 
-export default App;
+export default Home;
