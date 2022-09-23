c8483c1b53ae7b4ac496b34f63722a9e09c1d9e8
raymondgamiao <royalrfgum@gmail.com>
2022-09-23 17:26:39 +0800

    added new project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -15,26 +15,59 @@ const Projects = () => {
     >
       <h1>Projects</h1>
       <div className="projectsWrap">
-        <div className="ProjectCard" id="project2">
+        <div className="ProjectCard" id="project5">
           <div className="ProjectDetails">
             <div className="ProjectImg"></div>
-            <h2 className="ProjectTitle">Pre-Bootcamp Portfolio</h2>
-            <span className="ProjectType">Static Webpage</span>
+            <h2 className="ProjectTitle">PhotoCity</h2>
+            <span className="ProjectType">FullStack Booking System</span>
             <p className="ProjectDesc">
-              This is my previous portfolio before learning web development. It
-              showcases my skills as a graphic designer
+              Booking with Inventory and Employee management systems for a Photo
+              Video Company wit multiple branches
+            </p>
+            <ul className="ProjectStacks">
+              <li>Laravel</li>
+              <li>HTML</li>
+              <li>Jquery</li>
+              <li>Javascript</li>
+              <li>MySQL</li>
+            </ul>
+          </div>
+          <div className="Footer">
+            {" "}
+            <a
+              className="btn btn-primary"
+              href="https://github.com/raymondgamiao/capstone"
+              target={"_blank"}
+            >
+              Repo
+              <AiFillGithub className="ms-2" />
+            </a>
+          </div>
+        </div>
+
+        <div className="ProjectCard" id="project4">
+          <div className="ProjectDetails">
+            <div className="ProjectImg"></div>
+            <h2 className="ProjectTitle">Legend.GG</h2>
+            <span className="ProjectType">API Consuming Webpage</span>
+            <p className="ProjectDesc">
+              This is a companion web app for League of Legend players. Note:
+              You have to clone my repo and insert your own Riot API key to make
+              all features work
             </p>
             <ul className="ProjectStacks">
               <li>HTML</li>
               <li>CSS</li>
               <li>Bootstrap</li>
               <li>Javascript</li>
+              <li>RIOT API</li>
             </ul>
           </div>
           <div className="Footer">
+            {" "}
             <a
               className="btn btn-primary"
-              href="https://github.com/raymondgamiao/Mondi-Graphics"
+              href="https://github.com/raymondgamiao/m2"
               target={"_blank"}
             >
               Repo
@@ -42,7 +75,7 @@ const Projects = () => {
             </a>
             <a
               className="btn btn-secondary"
-              href="https://raymondgamiao.github.io/Mondi-Graphics/"
+              href="https://raymondgamiao.github.io/m2/"
               target={"_blank"}
             >
               Live Demo
@@ -87,29 +120,26 @@ const Projects = () => {
           </div>
         </div>
 
-        <div className="ProjectCard" id="project4">
+        <div className="ProjectCard" id="project2">
           <div className="ProjectDetails">
             <div className="ProjectImg"></div>
-            <h2 className="ProjectTitle">Legend.GG</h2>
-            <span className="ProjectType">API Consuming Webpage</span>
+            <h2 className="ProjectTitle">Pre-Bootcamp Portfolio</h2>
+            <span className="ProjectType">Static Webpage</span>
             <p className="ProjectDesc">
-              This is a companion web app for League of Legend players. Note:
-              You have to clone my repo and insert your own Riot API key to make
-              all features work
+              This is my previous portfolio before learning web development. It
+              showcases my skills as a graphic designer
             </p>
             <ul className="ProjectStacks">
               <li>HTML</li>
               <li>CSS</li>
               <li>Bootstrap</li>
               <li>Javascript</li>
-              <li>RIOT API</li>
             </ul>
           </div>
           <div className="Footer">
-            {" "}
             <a
               className="btn btn-primary"
-              href="https://github.com/raymondgamiao/m2"
+              href="https://github.com/raymondgamiao/Mondi-Graphics"
               target={"_blank"}
             >
               Repo
@@ -117,7 +147,7 @@ const Projects = () => {
             </a>
             <a
               className="btn btn-secondary"
-              href="https://raymondgamiao.github.io/m2/"
+              href="https://raymondgamiao.github.io/Mondi-Graphics/"
               target={"_blank"}
             >
               Live Demo
